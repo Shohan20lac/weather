698f8d0960228624ed6dea5350976590c8356fb9
Shohan20lac <r.shohan12@gmail.com>
2021-05-05 20:14:02 +0600

    Update formHandler.php

--- formHandler.php
+++ formHandler.php
@@ -20,15 +20,23 @@ function formSubmitted(){
 
 
 if (formSubmitted()){
-    $country = $_GET['country'];
-    $city = $_GET['city'];
-    $units = $_GET['units'];
-    echo 'Weather data for '.$city.', '.$country.' in '.$units.' units:' ;
+    $country = $_GET['country'];    // gets() the value from the "enter country name" frontend text field
+    $city = $_GET['city'];          // gets() the value from the "enter city" frontend text field
+    $units = $_GET['units'];        // gets() the value from the "select units" frontend text field
+    echo 'Weather data for '.$city.', '.$country.' in '.$units.' units:';
 
     $api_url = 'http://api.openweathermap.org/data/2.5/weather?q='.$city.','.$country.'&units='.$units.'&appid='.$api_key;
+    //packs all the variable choices into one URL 
 
     $weather_data = json_decode ( file_get_contents($api_url), true);
+    /*  - file_get_contents(http link) is used to get weather data from openweathermap in JSON format.
+        - json decode( fileInstance , true ) converts the file from json to array. 
+        - Watch out for the nested brackets. I had wasted ~2 hours because of typing nested brackets incorrectly.    */ 
 
+
+    /*  now that we have data from the json file stored in json format, we can just access them like a normal 2D array.
+        Optionally, you can use print_r($weather_data) to print the whole array first, 
+        and then decide which values to acces from it.*/
     $tempActual = $weather_data['main']['temp'];
     $tempFeel = $weather_data['main']['feels_like'];
     $tempMin = $weather_data['main']['temp_min'];
@@ -37,10 +45,9 @@ if (formSubmitted()){
     $windSpeed = $weather_data['wind']['speed'];
     $windDirection = $weather_data['wind']['deg'];
     $visibility = $weather_data['visibility'];
+  
 
-
-    
-
+    //with the data stored into primitve variables, we can make the frontend display them.
 }
 
 ?>
